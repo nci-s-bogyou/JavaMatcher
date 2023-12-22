4da5aa213b56b7775b35835d898cf98dc0c3f402
QMY\qmywe <my.qian@wesoft.co.jp>
2023-12-22 14:39:08 +0900

    1

--- Main.java
+++ Main.java
@@ -4,6 +4,45 @@ import java.util.regex.Pattern;
 
 public class Main {
 
+    private static void findJavaFiles(File xlsFolder, File javaFolder, FileWriter writer) throws IOException {
+        Pattern pattern = Pattern.compile("\\d+");
+
+        // 遍历XLS文件夹
+        for (File file : xlsFolder.listFiles()) {
+            if (file.isDirectory()) {
+                // 递归搜索子文件夹
+                findJavaFiles(file, javaFolder, writer);
+            } else if (file.getName().endsWith(".xls")) {
+                Matcher matcher = pattern.matcher(file.getName());
+                if (matcher.find()) {
+                    String number = matcher.group();
+                    File javaFile = new File(javaFolder, number + "Page.java");
+
+                    // 深度遍历Java文件夹以寻找匹配的文件
+                    if (findInJavaFolder(javaFile, javaFolder)) {
+                        writer.write("Found: " + javaFile.getPath() + "\n");
+                    } else {
+                        writer.write("Not Found: " + number + "Page.java\n");
+                    }
+                }
+            }
+        }
+    }
+
+    private static boolean findInJavaFolder(File javaFile, File folder) {
+        for (File file : folder.listFiles()) {
+            if (file.isDirectory()) {
+                // 递归搜索子文件夹
+                if (findInJavaFolder(javaFile, file)) {
+                    return true;
+                }
+            } else if (file.equals(javaFile)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
@@ -17,22 +56,7 @@ public class Main {
         File javaFolder = new File(javaFolderPath);
         FileWriter writer = new FileWriter("results.txt");
 
-        Pattern pattern = Pattern.compile("\\d+");
-
-        for (File file : xlsFolder.listFiles()) {
-            if (file.getName().endsWith(".xls")) {
-                Matcher matcher = pattern.matcher(file.getName());
-                if (matcher.find()) {
-                    String number = matcher.group();
-                    File javaFile = new File(javaFolder, number + "Page.java");
-                    if (javaFile.exists()) {
-                        writer.write("Found: " + javaFile.getPath() + "\n");
-                    } else {
-                        writer.write("Not Found: " + number + "Page.java\n");
-                    }
-                }
-            }
-        }
+        findJavaFiles(xlsFolder, javaFolder, writer);
 
         writer.close();
     }
